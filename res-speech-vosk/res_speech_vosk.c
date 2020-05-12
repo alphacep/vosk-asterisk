e34660378770fca4afbfe580a79c901fc1dc3b57
Nickolay Shmyrev <nshmyrev@gmail.com>
2020-05-12 02:21:48 +0200

    Initial adaptation

---
+++ res-speech-vosk/res_speech_vosk.c
@@ -0,0 +1,256 @@
+/*
+ * Asterisk -- An open source telephony toolkit.
+ *
+ * See http://www.asterisk.org for more information about
+ * the Asterisk project. Please do not directly contact
+ * any of the maintainers of this project for assistance;
+ * the project provides a web site, mailing lists and IRC
+ * channels for your use.
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE file
+ * at the top of the source tree.
+ *
+ * Please follow coding guidelines 
+ * http://svn.digium.com/view/asterisk/trunk/doc/CODING-GUIDELINES
+ */
+
+/*! \file
+ *
+ * \brief Implementation of the Asterisk's Speech API via Vosk
+ *
+ * \author Nickolay V. Shmyrev <nshmyrev@alphacephei.com>
+ * 
+ * \ingroup applications
+ */
+
+/* Asterisk includes. */
+#include "asterisk.h"
+#include "asterisk/logger.h"
+#include "asterisk/channel.h"
+
+#define AST_MODULE "res_speech_vosk"
+#include <asterisk/module.h>
+#include <asterisk/config.h>
+#include <asterisk/frame.h>
+#include <asterisk/speech.h>
+#include <asterisk/format_cache.h>
+
+#define VOSK_ENGINE_NAME "vosk"
+#define VOSK_ENGINE_CONFIG "res-speech-vosk.conf"
+
+/** \brief Forward declaration of speech (client object) */
+typedef struct vosk_speech_t vosk_speech_t;
+/** \brief Forward declaration of engine (global object) */
+typedef struct vosk_engine_t vosk_engine_t;
+
+/** \brief Declaration of UniMRCP based speech structure */
+struct vosk_speech_t {
+	/* Name of the speech object to be used for logging */
+	char                   *name;
+
+	/* Asterisk speech base */
+	struct ast_speech     *speech_base;
+	/* Recognition status */
+	int                    is_inprogress;
+};
+
+/** \brief Declaration of UniMRCP based recognition engine */
+struct vosk_engine_t {
+	/* Log level */
+	int			log_level;
+};
+
+static struct vosk_engine_t vosk_engine;
+
+/** \brief Set up the speech structure within the engine */
+static int vosk_recog_create(struct ast_speech *speech, struct ast_format *format)
+{
+	vosk_speech_t *vosk_speech;
+
+	vosk_speech = ast_malloc(sizeof(vosk_speech_t));
+	vosk_speech->name = "vosk";
+	vosk_speech->speech_base = speech;
+	speech->data = vosk_speech;
+
+	ast_log(LOG_NOTICE, "(%s) Create speech resource\n",vosk_speech->name);
+}
+
+/** \brief Destroy any data set on the speech structure by the engine */
+static int vosk_recog_destroy(struct ast_speech *speech)
+{
+	vosk_speech_t *vosk_speech = speech->data;
+	ast_log(LOG_NOTICE, "(%s) Destroy speech resource\n",vosk_speech->name);
+	ast_free(vosk_speech->name);
+	return 0;
+}
+
+/*! \brief Stop the in-progress recognition */
+static int vosk_recog_stop(struct ast_speech *speech)
+{
+	vosk_speech_t *vosk_speech = speech->data;
+	ast_log(LOG_NOTICE, "(%s) Stop recognition\n",vosk_speech->name);
+	return 0;
+}
+
+/*! \brief Load a local grammar on the speech structure */
+static int vosk_recog_load_grammar(struct ast_speech *speech, const char *grammar_name, const char *grammar_path)
+{
+	return 0;
+}
+
+/** \brief Unload a local grammar */
+static int vosk_recog_unload_grammar(struct ast_speech *speech, const char *grammar_name)
+{
+	return 0;
+}
+
+/** \brief Activate a loaded grammar */
+static int vosk_recog_activate_grammar(struct ast_speech *speech, const char *grammar_name)
+{
+	return 0;
+}
+
+/** \brief Deactivate a loaded grammar */
+static int vosk_recog_deactivate_grammar(struct ast_speech *speech, const char *grammar_name)
+{
+	return 0;
+}
+
+/** \brief Write audio to the speech engine */
+static int vosk_recog_write(struct ast_speech *speech, void *data, int len)
+{
+	vosk_speech_t *vosk_speech = speech->data;
+
+#if 1
+	ast_log(LOG_DEBUG, "(%s) Write audio len: %d\n",vosk_speech->name,len);
+#endif
+	return 0;
+}
+
+/** \brief Signal DTMF was received */
+static int vosk_recog_dtmf(struct ast_speech *speech, const char *dtmf)
+{
+	vosk_speech_t *vosk_speech = speech->data;
+	ast_log(LOG_NOTICE, "(%s) Signal DTMF %s\n",vosk_speech->name,dtmf);
+	return 0;
+}
+
+/** brief Prepare engine to accept audio */
+static int vosk_recog_start(struct ast_speech *speech)
+{
+	vosk_speech_t *vosk_speech = speech->data;
+
+	ast_log(LOG_NOTICE, "(%s) Start recognition\n",vosk_speech->name);
+	return 0;
+}
+
+/** \brief Change an engine specific setting */
+static int vosk_recog_change(struct ast_speech *speech, const char *name, const char *value)
+{
+	vosk_speech_t *vosk_speech = speech->data;
+	ast_log(LOG_NOTICE, "(%s) Change setting name: %s value:%s\n",vosk_speech->name,name,value);
+	return 0;
+}
+
+/** \brief Get an engine specific attribute */
+static int vosk_recog_get_settings(struct ast_speech *speech, const char *name, char *buf, size_t len)
+{
+	vosk_speech_t *vosk_speech = speech->data;
+	ast_log(LOG_NOTICE, "(%s) Get settings name: %s\n",vosk_speech->name,name);
+	return -1;
+}
+
+/** \brief Change the type of results we want back */
+static int vosk_recog_change_results_type(struct ast_speech *speech,enum ast_speech_results_type results_type)
+{
+	return -1;
+}
+
+
+/** \brief Try to get result */
+struct ast_speech_result* vosk_recog_get(struct ast_speech *speech)
+{
+	struct ast_speech_result *result;
+
+	vosk_speech_t *vosk_speech = speech->data;
+
+	if(vosk_speech->is_inprogress) {
+		vosk_recog_stop(speech);
+	}
+	ast_set_flag(speech,AST_SPEECH_HAVE_RESULTS);
+	return result;
+}
+
+/** \brief Speech engine declaration */
+static struct ast_speech_engine ast_engine = {
+	VOSK_ENGINE_NAME,
+	vosk_recog_create,
+	vosk_recog_destroy,
+	vosk_recog_load_grammar,
+	vosk_recog_unload_grammar,
+	vosk_recog_activate_grammar,
+	vosk_recog_deactivate_grammar,
+	vosk_recog_write,
+	vosk_recog_dtmf,
+	vosk_recog_start,
+	vosk_recog_change,
+	vosk_recog_get_settings,
+	vosk_recog_change_results_type,
+	vosk_recog_get
+};
+
+/** \brief Load UniMRCP engine configuration (/etc/asterisk/res_speech_vosk.conf)*/
+static int vosk_engine_config_load()
+{
+	const char *value = NULL;
+	struct ast_flags config_flags = { 0 };
+	struct ast_config *cfg = ast_config_load(VOSK_ENGINE_CONFIG, config_flags);
+	if(!cfg) {
+		ast_log(LOG_WARNING, "No such configuration file %s\n", VOSK_ENGINE_CONFIG);
+		return -1;
+	}
+	if((value = ast_variable_retrieve(cfg, "general", "log-level")) != NULL) {
+		ast_log(LOG_DEBUG, "general.log-level=%s\n", value);
+		vosk_engine.log_level = atoi(value);
+	}
+	ast_config_destroy(cfg);
+	return 0;
+}
+
+/** \brief Load module */
+static int load_module(void)
+{
+	ast_log(LOG_NOTICE, "Load Res-Speech-UniMRCP module\n");
+
+	vosk_engine.log_level = LOG_DEBUG;
+
+	/* Load engine configuration */
+	vosk_engine_config_load();
+
+	ast_engine.formats = ast_format_cap_alloc(AST_FORMAT_CAP_FLAG_DEFAULT);
+	if(!ast_engine.formats) {
+		ast_log(LOG_ERROR, "Failed to alloc media format capabilities\n");
+		return AST_MODULE_LOAD_FAILURE;
+	}
+	ast_format_cap_append(ast_engine.formats, ast_format_slin, 0);
+
+	if(ast_speech_register(&ast_engine)) {
+		ast_log(LOG_ERROR, "Failed to register module\n");
+		return AST_MODULE_LOAD_FAILURE;
+	}
+
+	return AST_MODULE_LOAD_SUCCESS;
+}
+
+/** \brief Unload module */
+static int unload_module(void)
+{
+	ast_log(LOG_NOTICE, "Unload Res-Speech-Vosk module\n");
+	if(ast_speech_unregister(VOSK_ENGINE_NAME)) {
+		ast_log(LOG_ERROR, "Failed to unregister module\n");
+	}
+	return 0;
+}
+
+AST_MODULE_INFO_STANDARD(ASTERISK_GPL_KEY, "Vosk Speech Engine");
