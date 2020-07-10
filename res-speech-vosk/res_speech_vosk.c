c23b1efc14a676deeb7f7b57635459253901cf70
Ivan Batalin <morganhold@ya.ru>
2020-07-10 13:21:01 +0500

    Convert grammar to the base64

--- res-speech-vosk/res_speech_vosk.c
+++ res-speech-vosk/res_speech_vosk.c
@@ -105,6 +105,46 @@ struct vosk_engine_t {
 
 static struct vosk_engine_t vosk_engine;
 
+static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                                'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                                'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+                                'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                'w', 'x', 'y', 'z', '0', '1', '2', '3',
+                                '4', '5', '6', '7', '8', '9', '+', '/'};
+
+static int mod_table[] = {0, 2, 1};
+
+char *base64_encode(const unsigned char *data,
+                    int input_length,
+                    int *output_length) {
+
+    *output_length = 4 * ((input_length + 2) / 3);
+
+    char *encoded_data = ast_malloc(*output_length);
+    if (encoded_data == NULL) return NULL;
+
+    for (int i = 0, j = 0; i < input_length;) {
+
+        uint32_t octet_a = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_b = i < input_length ? (unsigned char)data[i++] : 0;
+        uint32_t octet_c = i < input_length ? (unsigned char)data[i++] : 0;
+
+        uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+
+        encoded_data[j++] = encoding_table[(triple >> 3 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 2 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 1 * 6) & 0x3F];
+        encoded_data[j++] = encoding_table[(triple >> 0 * 6) & 0x3F];
+    }
+
+    for (int i = 0; i < mod_table[input_length % 3]; i++)
+        encoded_data[*output_length - 1 - i] = '=';
+
+    return encoded_data;
+}
+
 /*! \brief Search list of servers and find corresponding one */
 static const char* _server_list_get(char* server)
 {
@@ -204,7 +244,7 @@ static int vosk_recog_stop(struct ast_speech *speech)
 /** \brief Send grammar data to add named grammar and policy to the Vosk server */
 static int vosk_load_ws_grammar(struct ast_websocket *ws, const char *grammar_name, const char *file_name) {
 	int len = 0, result = 0, fd, file_size;
-	char *buf =	NULL, *file_contents = NULL;
+	char *buf =	NULL, *file_contents = NULL, *base64 = NULL;
     if (!ast_file_is_readable(file_name)) return -1;
     fd = open(file_name, O_RDONLY);
 	if (fd == -1) return -1;
@@ -223,6 +263,14 @@ static int vosk_load_ws_grammar(struct ast_websocket *ws, const char *grammar_na
 	}
 	file_size = read(fd, file_contents, file_size);
 	close(fd);
+    
+	base64 = base64_encode(file_contents, file_size, &file_size);
+	ast_free(file_contents);
+	if(base64 == NULL) {
+		return -1;
+	}
+    file_contents = base64;
+
 	ast_log(LOG_NOTICE, "(%s) Upload grammar to engine over websocket: %s \n", "vosk", grammar_name);
 	len = snprintf(buf, len, "{\"newgrammar\": \"%s\", \"grammar_data\": \"%s\"}", grammar_name, file_contents);
 	if (len < 0) {
